a0f46945431941792f39f4230491329537c1e04a
Dave Tarrant <davetaz@theodi.org>
2020-01-23 14:11:01 +0000

    Adding story

--- js/buildFramework.js
+++ js/buildFramework.js
@@ -1,4 +1,5 @@
 function populateFramework(file) {
+	$('#currentFramework').val(file);
 	d3.csv("config/" + file + ".csv", function(data) {
 		split = data.Title.split(' ');
 		topTitle = split[0];
@@ -18,10 +19,41 @@ function populateFramework(file) {
 			bottomTitle = bottomTitle.trim();
 		}
 		$('#row_' + data.Row + ' > #column_' + data.Column).css('opacity',data['Hex opacity']);
-		$('#row_' + data.Row + ' > #column_' + data.Column + ' > .hexagon').addClass(data.Area);
-		$('#row_' + data.Row + ' > #column_' + data.Column + ' > .hexagon').append("<h4>" + topTitle + "<br/>" + bottomTitle + "</h4>");
-		$('#row_' + data.Row + ' > #column_' + data.Column).prepend($('<img>',{src:'assets-white/' + data.Icon,alt:data.Title,style:data["Icon style"]}));
+		if (parseInt(data.Row) != 6) {
+			$('#row_' + data.Row + ' > #column_' + data.Column + ' > .hexagon').addClass(data.Area);
+			$('#row_' + data.Row + ' > #column_' + data.Column + ' > .hexagon').append("<h4>" + topTitle + "<br/>" + bottomTitle + "</h4>");
+			$('#row_' + data.Row + ' > #column_' + data.Column).prepend($('<img>',{src:'assets-white/' + data.Icon,alt:data.Title,style:data["Icon style"]}));
+		}
 
+	});
+}
+
+function repopulateFramework(file) {
+	$('#currentFramework').val(file);
+	d3.csv("config/" + file + ".csv", function(data) {
+		split = data.Title.split(' ');
+		topTitle = split[0];
+		bottomTitle = split[1];
+		if (split.length > 2) {
+			topTitle = split[0];
+			bottomTitle = "";
+			for (i=1;i<split.length;i++) {
+				console.log(topTitle);
+				if (topTitle.length < 12 && (topTitle + " " + split[i]).length < 12) {
+					topTitle += ' ' + split[i];
+				} else {
+					bottomTitle += ' ' + split[i];
+				}
+			}
+			topTitle = topTitle.trim();
+			bottomTitle = bottomTitle.trim();
+		}
+		
+		$('#row_' + data.Row + ' > #column_' + data.Column).css('opacity',data['Hex opacity']);		
+		//} else {
+			
+		//}
+		
 	});
 }
 
@@ -30,3 +62,4 @@ $( document ).ready(function() {
 	const myParam = urlParams.get('framework');
 	populateFramework(myParam);
 });
+
